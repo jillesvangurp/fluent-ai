928f0121369fa406d1031264a1a0b0085f3f68b3
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2024-06-01 16:22:28 +0200

    implement grouping by common prefix

--- src/commonMain/kotlin/com/jillesvangurp/fluentai/FluentFile.kt
+++ src/commonMain/kotlin/com/jillesvangurp/fluentai/FluentFile.kt
@@ -1,5 +1,6 @@
 package com.jillesvangurp.fluentai
 
+import kotlin.math.max
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
@@ -8,7 +9,8 @@ data class FluentFile(val name: String, private var _content: String) {
     val content get() = _content
 
     @Transient
-    private val fluentDefinitionRegex = Regex("""^\s*([a-zA-Z0-9_-]+)\s*=\s*(.*(\n[^#]\s+.*)*)""", RegexOption.MULTILINE)
+    private val fluentDefinitionRegex =
+        Regex("""^\s*([a-zA-Z0-9_-]+)\s*=\s*(.*(\n[^#]\s+.*)*)""", RegexOption.MULTILINE)
 
     operator fun get(key: String): String? {
         return asMap()[key]
@@ -35,6 +37,58 @@ data class FluentFile(val name: String, private var _content: String) {
     }
 
     fun asMap(): Map<String, String> {
-        return fluentDefinitionRegex.findAll(_content).associate { it.groupValues[1].trim() to it.groupValues[2].trim() }
+        return fluentDefinitionRegex.findAll(_content)
+            .associate { it.groupValues[1].trim() to it.groupValues[2].trim() }
+    }
+
+    fun matches(language: String): Boolean {
+        val normalized = name.lowercase().replace(".ftl", "").replace("_", "-")
+        return normalized.contains(language.lowercase().replace(".ftl", "").replace("_", "-"))
+    }
+
+    fun groupByLargestPrefix(): Map<String, Map<String, String>> {
+        val map = asMap()
+        val prefixes = mutableMapOf<String, MutableList<String>>()
+
+        val keys = map.keys.toMutableSet()
+        while (keys.isNotEmpty()) {
+            val current = keys.first()
+            if (current.startsWith("-")) {
+                if ("" !in prefixes) {
+                    prefixes[""] = mutableListOf()
+                }
+                prefixes[""]!!.add(current)
+                keys.remove(current)
+            } else {
+                val splitted = current.split("-")
+                var end = splitted.size - 1
+                var prefix = ""
+                var found = listOf<String>()
+                while (end > 0 && found.size <= 1) {
+                    prefix = splitted.subList(0, end).joinToString("-")
+                    found = keys.filter {
+                        if (prefix.isBlank()) {
+                            it == current
+                        } else {
+                            it.startsWith(prefix)
+                        }
+                    }
+                    end--
+                }
+                keys -= found.toSet()
+                if (found.size == 1) {
+                    prefix = ""
+                }
+                if (prefix !in prefixes) {
+                    prefixes[prefix] = mutableListOf()
+                }
+                prefixes[prefix]!!.addAll(found)
+            }
+        }
+
+        return prefixes.map { (p, idList) ->
+            val es = map.entries.filter { (id, value) -> id in idList }
+            p to es.associate { (k, v) -> k to v }
+        }.toMap()
     }
 }
--- src/commonTest/kotlin/com/jillesvangurp/fluentai/FluentFileTest.kt
+++ src/commonTest/kotlin/com/jillesvangurp/fluentai/FluentFileTest.kt
@@ -1,5 +1,8 @@
 package com.jillesvangurp.fluentai
 
+import io.kotest.matchers.collections.shouldHaveAtLeastSize
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.maps.shouldHaveSize
 import kotlin.test.Test
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
@@ -81,5 +84,50 @@ class FluentFileTest {
             "foo" to "First line\n     Second line",
             "bar" to "Another value"
         )
+    }
+
+    @Test
+    fun shouldGroupByCommonPrefix() {
+        val content ="""
+            -brand = Fluent Translations AI
+
+            common-cancel = Cancel
+            common-clear = Clear
+            common-confirm = OK
+            common-filter-placeholder = Filter the list
+            common-save = Save
+
+            file-loader-drag-and-drop = Drag ftl files here to load them
+            file-loader-load-own-ftls = Load ftl files for { -brand }
+
+            language-select-select-language = Pick a language
+
+            pages-editor = Edit Fluent Definitions
+            pages-files = Manage Fluent Files
+            pages-settings = Settings
+
+            settings-open-ai-key = Set your OpenAI API Key (needed for translations)
+            settings-translation-language = Configure source language for AI translations. Note, the language is matched against your ftl file name with some normalization. Defaults to en-US.
+
+        """.trimIndent()
+        val fluentFile = FluentFile("example.ftl", content)
+        val grouped = fluentFile.groupByLargestPrefix()
+//        console.log(grouped.keys.joinToString(","))
+
+//        grouped.forEach {(pre,map) ->
+//            map.forEach { (id,  value) ->
+//                println("$pre: $id")
+//            }
+//        }
+
+        grouped.keys shouldHaveAtLeastSize 5
+        grouped[""]!!.entries shouldHaveSize 2
+        grouped["file-loader"]!!.entries shouldHaveSize 2
+        grouped["pages"]!!.entries shouldHaveSize 3
+        grouped.forEach { (prefix,map) ->
+            map.keys.forEach {
+                it shouldStartWith prefix
+            }
+        }
     }
 }
