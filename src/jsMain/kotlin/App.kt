15d598b75ec3a7279151f2c47aa7250858d2ea37
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2024-06-08 12:15:18 +0200

    add exception handling

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -3,17 +3,20 @@ import dev.fritz2.headless.foundation.portalRoot
 import routing.mainScreen
 
 suspend fun main() {
-//    setJsLogLevel(JsLogLevel.INFO)
-    // little hack to get this to load in a co-routine scope because resource loading is suspending
-    startAppWithKoin {
-        mainScreen()
+    try {//    setJsLogLevel(JsLogLevel.INFO)
+        // little hack to get this to load in a co-routine scope because resource loading is suspending
+        startAppWithKoin {
+            mainScreen()
 
-        // mount points for fritz2 stuff
-        toastContainer(
-            "messages", // name
-            "absolute bottom-5 left-1/2 -translate-x-48 mx-auto flex flex-col gap-2 place-items-center w-96",
-        )
-        portalRoot()
+            // mount points for fritz2 stuff
+            toastContainer(
+                "messages", // name
+                "absolute bottom-5 left-1/2 -translate-x-48 mx-auto flex flex-col gap-2 place-items-center w-96",
+            )
+            portalRoot()
+        }
+    } catch (e: Exception) {
+        console.error("Error loading the app", e)
     }
 }
 
